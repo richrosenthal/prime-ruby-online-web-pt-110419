11c4f975e02a204304b413adc1ec86431f71db15
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-22 17:23:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,17 @@
 def prime?(number) 
 true_or_false = true
 array_of_numbers = []
+count = 1
 
   if number < 0 
     return false 
   end
   
-number.times   
+number.times do 
+  puts count
+  count += 1 
+end
+
 true_or_false
 end 
   

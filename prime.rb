4714275beeada1d5d84af1a1a3c6b5a286d9dce7
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-22 17:15:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+# Add  code here!defprime?()
+def prime?(number) 
+if number <= 1 
+  return false
+end
+ if number%2 == 0 
+  if number == 2 
+     return true
+   else 
+     return false
+    end 
+end
+if number%2 > 0 
+  return true 
+end
+
+end 
+  

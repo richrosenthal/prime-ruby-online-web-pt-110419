9fdb58883fc7ce42e1062e55d0b29d1fddbdb3d9
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-22 17:42:30 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Implementing Prime
+f# Implementing Prime
 
 ## Objectives
 
--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
-# Add  code here!
+# Add  code here!defprime?()
+def prime?(number) 
+true_or_false = true
+array_of_numbers = []
+count = 2
+
+  if number < 2 
+    true_or_false = false 
+    return false 
+  end
+  
+number.times do 
+    if count != number 
+      array_of_numbers << count
+    end
+  count += 1 
+end
+ array_of_numbers.pop
+
+
+array_of_numbers.each do |integer|
+  if (number % integer ) == 0 
+    return false 
+  end
+ 
+ end 
+true_or_false
+end 
+  
